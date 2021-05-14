70d1884e78e4e1664ad10e7f81235908ad7288f2
Papp Gergely <gergopool@gmail.com>
2021-05-14 15:33:14 +0200

    bugfix

--- grid_run-10-19.sh
+++ grid_run-10-19.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in {10..19}
 do
-    CUDA_VISIBLE_DEVICES=$((i/10)) python src/bin/train.py config/train/cka_alter/in$i-gn$i.ini > $i.out 
+    CUDA_VISIBLE_DEVICES=$((i/10)) python src/bin/train_torch.py config/train/cka_alter/in$i-gn$i.ini > $i.out 
 done
